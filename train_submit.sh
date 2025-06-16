918766de61973af340b673d51868ea840664b0c1
Sehwan Yoo <krstyle03v@gmail.com>
2025-06-16 09:49:19 +0800

    Add files via upload
    Name changed

--- train_submit.sh
+++ train_submit.sh
@@ -4,7 +4,7 @@
 #PBS -l select=1:ncpus=64:ngpus=4:mem=440gb 
 #PBS -l walltime=50:00:00
 #PBS -P personal-sehwan00
-#PBS -N log_train_vae
+#PBS -N log_train_gan
 # export NCCL_SOCKET_IFNAME=enp8s0
 export NCCL_DEBUG=INFO
 # Commands start here
