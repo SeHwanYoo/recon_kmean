0737e61d42296de2569baae58382b9e82610783d
Sehwan <krstyle03v@gmail.com>
2025-06-16 19:54:58 +0800

    ..

--- make_masks.sh
+++ make_masks.sh
@@ -3,7 +3,7 @@
 #PBS -j oe
 #PBS -l select=1:ncpus=64:ngpus=1:mem=440gb 
 #PBS -l walltime=10:00:00
-#PBS -P personal-sehwan00
+#PBS -P personal-yoosehwa
 #PBS -N log_train_gan
 # export NCCL_SOCKET_IFNAME=enp8s0
 export NCCL_DEBUG=INFO
